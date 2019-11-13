84c1eec81bbcc538a9d18939fce72886c8eea6fe
morgothsvoidpointer <54783192+morgothsvoidpointer@users.noreply.github.com>
2019-11-13 07:58:13 +0000

    Add files via upload

--- friend_unfriend2.py
+++ friend_unfriend2.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Редактор Spyder
-
 Это временный скриптовый файл.
 """
 
@@ -10,41 +9,46 @@ import SocialistSunday
 import tweepy
 import twitter_orig_utils
 
+import sys
+import os
+from twitter_orig_utils import *
+from twitter_authorise import api, auth, save_dir, userhome, python_dir
 
-consumer_key = 'Xa8XbijTP7F3sg0QJ1QUoLq6f'
-consumer_secret = 'cRBjxlQK3PO2cy4zFnfZGlLdtIYm1c9KMIe8IsZVaE0OqFIMsh'
-access_token = '1092850952284131328-6prMHiiBPsQHU5In4xho9N1cQy9wpO'
-access_token_secret = 'BOs3QPs7PnF8uRMgaexuv3BizPPkMeSdXq3YEqATcIsM9'
+#users not to touch:
 
-query1="#SocialistSunday -filter:retweets"
-query2="#SocialistAnyDay -filter:retweets"
+special_users=["OwenJones84","paulmasonnews","graceblakeley","AnnPettifor","JacDunc1","Alston_UNSR","JamesEFoster","sylviatopp","nbissell","jennirsl"]
+
+special_orgs=["NHSMillion","LabourList","tribunemagazine","LeftieStats","ByDonkeys","Orwell_Society"]
+
+special_users.extend(special_orgs)
 
 
 
-# OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
- 
-# Creation of the actual interface, using authentication
-api = tweepy.API(auth,wait_on_rate_limit=True) 
+#query1="#SocialistSunday -filter:retweets"
+#query2="#SocialistAnyDay -filter:retweets"
+
+max_follower_count2=10000
+
 
 def get_follower_list(username):
     follower_list=[]
-    for user in tweepy.Cursor(api.followers, screen_name=username, count=200).items():
-        follower_list.append(user.screen_name)
-        print(user.screen_name)
-        print(len(follower_list))
+    for users in tweepy.Cursor(api.followers, screen_name=username, count=200).pages():
+        for user in users:
+            follower_list.append(user.screen_name)
+            print(user.screen_name)
+            print(len(follower_list))
     return(follower_list)
 
 def get_friend_list(username):
     friend_list=[]
-    for user in tweepy.Cursor(api.friends, screen_name=username, count=200).items():
-        friend_list.append(user.screen_name)
-        print(user.screen_name)
-        print(len(friend_list))
-    return(friend_list)    
+    for users in tweepy.Cursor(api.friends, screen_name=username, count=200).pages():
+        for user in users:
+            friend_list.append(user.screen_name)
+            print(user.screen_name)
+            print(len(friend_list))
+    return(friend_list)
 
-    
+#%%    
 #now unfollow people I follow who don't follow me, belong to the SS-list and don't have above a set number of friends.
     
 if __name__ == '__main__':
@@ -65,19 +69,35 @@ if __name__ == '__main__':
     #intersect bad friends with social sunday list
     keylist=list_flat(array_load(save_dir+"socialist_sunday_list_old.csv"))
     very_bad_friends=[]
+    
+    if os.path.exists(save_dir+"socialist_sunday_list_superusers.csv"):
+        superusers=list_flat(array_load(save_dir+"socialist_sunday_list_superusers.csv"))
+    
     for bad_friend in bad_friends:
-        if bad_friend in keylist:
-            very_bad_friends.append(bad_friend)
+        if '@'+bad_friend in keylist:
+            if not os.path.exists(save_dir+"socialist_sunday_list_superusers.csv"):
+                try:
+                    user_get=api.get_user(screen_name=bad_friend)
+                except tweepy.TweepError:
+                    print("user "+bad_friend+" threw error")
+                    continue
+                if user_get.followers_count<max_follower_count2 and user_get.verified==False:
+                    very_bad_friends.append(bad_friend)
+            else:
+                if bad_friend not in superusers:
+                    very_bad_friends.append(bad_friend)
     
+    #remove special users
     
-    #exclude people with too many followers
-            
-    #already done
-
+    small_bad_friends=[]
+    for bad_friend in bad_friends:
+        if bad_friend not in special_users:
+            small_bad_friends.append(bad_friend)
+           
+    list_write(save_dir+"very_bad_friends.csv",small_bad_friends)
+#%% 
+    import py_compile 
+    py_compile.compile('SocialistSunday.py')
+    from SocialistSunday import *
     #now unfollow
-    final=SocialistSunday.user_unfollow(Friends,very_bad_friends)
-    
-
-        
-            
-            
+    final=users_unfollow(Friends,small_bad_friends)
